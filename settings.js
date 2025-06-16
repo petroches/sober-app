45aa32ea7cfe5ba2ff034827c514b1d03ca47d08
petroches <petroches@gmail.com>
2025-06-16 14:09:43 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,15 @@
 function goBack() {
   window.location.href = "index.html";
 }
+
+const allControls = document.querySelectorAll('.quantity-control');
+
+allControls.forEach(control => {
+  const options = control.querySelectorAll('.quantity-option');
+  options.forEach(option => {
+    option.addEventListener('click', () => {
+      options.forEach(o => o.classList.remove('selected'));
+      option.classList.add('selected');
+    });
+  });
+});
